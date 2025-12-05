9ba24fb985addb0d9aeebcf55856e8e646b0c8f3
John Singer <john@singerlinks.com>
2025-12-05 12:42:35 -0600

    fix: updated __init__.py

--- src/rpi_app_framework/__init__.py
+++ src/rpi_app_framework/__init__.py
@@ -4,6 +4,7 @@ from .led_simple import LEDSimple
 from .wifi_manager import WiFiManager
 from .motor_driver_tb6612 import MotorDriverTB6612FNG
 from .microdot_manager import MicrodotManager
+from .hardware import PiHardwareAdapter
 
-__version__ = "0.1.0"
-__all__ = ['RPIApp', 'DeviceManager', 'LEDSimple', 'WiFiManager', 'MotorDriverTB6612FNG', 'MicrodotManager']
+__version__ = "0.1.1"
+__all__ = ['RPIApp', 'DeviceManager', 'LEDSimple', 'WiFiManager', 'MotorDriverTB6612FNG', 'MicrodotManager','PiHardwareAdapter']
