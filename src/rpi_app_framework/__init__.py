8ef1b0fbe37a8379820aac49ae2e441391bdba80
John Singer <john@singerlinks.com>
2025-12-14 08:54:22 -0500

    fix(imports): fix imports

--- src/rpi_app_framework/__init__.py
+++ src/rpi_app_framework/__init__.py
@@ -1,10 +1,23 @@
-from .rpi_app import RPIApp
-from .device_manager import DeviceManager
-from .led_simple import LEDSimple
-from .wifi_manager import WiFiManager
-from .motor_driver_tb6612 import MotorDriverTB6612FNG
-from .microdot_manager import MicrodotManager
-from .hardware import PiHardwareAdapter
+# rpi_app_framework/__init__.py - Package initializer with absolute exports
+# This allows users to import directly from the package root, e.g.:
+# from rpi_app_framework import RPIApp, DeviceManager, LEDSimple, etc.
+# Avoids relative imports in user code (main.py) and prevents ImportError on Pico.
+
+from rpi_app_framework import RPIApp
+from rpi_app_framework import DeviceManager
+from rpi_app_framework import LEDSimple
+from rpi_app_framework import WiFiManager
+from rpi_app_framework import MotorDriverTB6612FNG
+from rpi_app_framework import MicrodotManager
+from rpi_app_framework import PiHardwareAdapter  # If you have this file
 
 __version__ = "0.1.1"
-__all__ = ['RPIApp', 'DeviceManager', 'LEDSimple', 'WiFiManager', 'MotorDriverTB6612FNG', 'MicrodotManager','PiHardwareAdapter']
+__all__ = [
+    'RPIApp',
+    'DeviceManager',
+    'LEDSimple',
+    'WiFiManager',
+    'MotorDriverTB6612FNG',
+    'MicrodotManager',
+    'PiHardwareAdapter'
+]
