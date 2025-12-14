4909791a10e6af1d92486489f1919d7486a4d722
John Singer <john@singerlinks.com>
2025-12-14 10:44:42 -0500

    fix: use relative imports for pico compatibility

--- src/rpi_app_framework/__init__.py
+++ src/rpi_app_framework/__init__.py
@@ -3,13 +3,13 @@
 # from rpi_app_framework import RPIApp, DeviceManager, LEDSimple, etc.
 # Avoids relative imports in user code (main.py) and prevents ImportError on Pico.
 
-from rpi_app_framework import RPIApp
-from rpi_app_framework import DeviceManager
-from rpi_app_framework import LEDSimple
-from rpi_app_framework import WiFiManager
-from rpi_app_framework import MotorDriverTB6612FNG
-from rpi_app_framework import MicrodotManager
-from rpi_app_framework import PiHardwareAdapter  # If you have this file
+from .rpi_app import RPIApp
+from .device_manager import DeviceManager
+from .led_simple import LEDSimple
+from .wifi_manager import WiFiManager
+from .motor_driver_tb6612 import MotorDriverTB6612FNG
+from .microdot_manager import MicrodotManager
+from .pi_hardware_adapter import PiHardwareAdapter  # If you have this file
 
 __version__ = "0.1.1"
 __all__ = [
